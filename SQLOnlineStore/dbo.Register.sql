7076e583607f2a5a1ed32d67d51b073b58ce7533
Dave <rrrrabigor@gmail.com>
2024-05-06 10:52:27 +0300

    Create dbo.Register.sql

---
+++ SQLOnlineStore/dbo.Register.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Register] (
+    [Id]            INT           IDENTITY (1, 1) NOT NULL,
+    [login_user]    NVARCHAR (50) NOT NULL,
+    [password_user] NVARCHAR (50) NOT NULL,
+    [is_admin] BIT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
